ea6b8ed9651152a7ae1e8ba67a67347aede1cf76
ali Mohammadi Barzegar <a_barzegar@modernisc.com>
2023-04-05 12:11:01 +0330

    ch5_2

---
+++ ch5_2/src/main/java/ir/mindsheets/ch5_2/Main.java
@@ -0,0 +1,24 @@
+package ir.mindsheets.ch5_2;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.*;
+
+public class Main extends AbstractVerticle {
+
+  public static void main(String[] args) {
+
+    Vertx vertx = Vertx.vertx();
+    
+    vertx.deployVerticle("ir.mindsheets.ch5_2.HeatSensor",
+        new DeploymentOptions().setConfig(new JsonObject().put("http.port", 3000)));
+    vertx.deployVerticle("ir.mindsheets.ch5_2.HeatSensor",
+        new DeploymentOptions().setConfig(new JsonObject().put("http.port", 3001)));
+    vertx.deployVerticle("ir.mindsheets.ch5_2.HeatSensor",
+        new DeploymentOptions().setConfig(new JsonObject().put("http.port", 3002)));
+    vertx.deployVerticle("ir.mindsheets.ch5_2.SnapshotService");
+    vertx.deployVerticle("ir.mindsheets.ch5_2.CollectorService");
+
+  }
+
+}
